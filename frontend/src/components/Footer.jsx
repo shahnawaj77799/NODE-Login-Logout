0ecda42111810468087cc7ae9bf8113920f36a77
shahnawaj ali <shahnawajali323@gmail.com>
2024-12-17 01:18:43 +0530

    first

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+
+
+
+
+
+const Footer=()=>{
+    return(
+        <>
+           <div id="myFooter">
+            
+             <h3> www.mylogin.com all right reserved</h3>
+            
+            </div>   
+        </>
+    )
+}
+
+export default Footer;
